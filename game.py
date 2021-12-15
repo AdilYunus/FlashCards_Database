aa1d24b38104cab9f5932f6f60adac943d1956c8
ad unus <adil.yunus@gmail.com>
2021-12-15 01:42:51 +0100

    game sayfasi

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-from PyQt5 import QtWidgets, uic, QtGui, QtCore
+from PyQt5 import QtWidgets, uic, QtGui, QtCore,QtTest
 from PyQt5.QtCore import QTime, QTimer
 import menu
 import sys
@@ -105,14 +105,33 @@ class Game(QtWidgets.QDialog):
             SELECT player_level from PLAYERS where player_name ='{self.name}'
             """)
         level_x = cur.fetchone()
+        print('level-x=',level_x[0])
+        print('self.level =',self.level)
         if level_x[0] < self.level:
             cur.execute(
                 f"UPDATE PLAYERS set player_level = {self.level} where player_name ='{self.name}' ")
+            print('112')
             cur.execute(
                 f"UPDATE PLAYERS set player_time = {self.count_t} where player_name ='{self.name}' ")
+            print('115')
+            
+
+
+            cur.execute(f"""SELECT player_name,CAST(AVG(level_percent) AS INTEGER) avg_percent 
+                            FROM levels 
+                            WHERE player_name = '{self.name}' 
+                            GROUP BY player_name;""")
+            my_avg = cur.fetchone()
+            cur.execute(
+                f"""UPDATE PLAYERS set player_level = {self.level},average_percent = {my_avg[1]},player_time = {self.count_t}
+                where player_name ='{self.name}' """)
+            print('126')
+
         else:
             cur.execute(
                 f"UPDATE PLAYERS set player_time = {self.count_t} where player_name ='{self.name}' ")
+            print('132')
+
         conn.commit()
         conn.close()
 
@@ -127,12 +146,16 @@ class Game(QtWidgets.QDialog):
         self.sleeptime.setText(str(self.count))
         self.index += 1
         self.c_true += 1
+
         if self.index > 20:
             self.c_false -= 1
 
         if self.c_true == 20:  # if c_true = 20 level +1 and begin new level
 
-            self.level += 1
+            self.percentupdate()
+            #self.level += 1
+            self.tebrik()#yeni eklendi
+
             self.c_true = 0
             self.c_false = 0
 
@@ -205,3 +228,84 @@ class Game(QtWidgets.QDialog):
 
         self.words.setText(self.word_list[self.index][0])
         self.language.setText('Nederlands')
+
+    def percentupdate(self):
+        conn = psycopg2.connect(database="FlashCards",
+                                user="postgres",
+                                password="1234",
+                                host="localhost",
+                                port="5432")
+        cur = conn.cursor()
+        cur.execute(f"""
+            SELECT player_level from PLAYERS where player_name ='{self.name}'
+            """)
+        level_x = cur.fetchone()
+
+        if level_x[0] == self.level:
+            # 1 updat levels table -click_count and percentage
+            # 2 insert new row to levels met level+=1 and nulls(click & percentage)
+            # 3 update players table player_level and time and AVG_percentage
+            cur.execute(
+                f"UPDATE levels set click_count = {self.index} where case when player_name ='{self.name}' and level_id = {self.level} then 0 else 1 end=0; ")
+            p = int(20/self.index*100)
+            cur.execute(
+                f"UPDATE levels set level_percent = {p} where case when player_name ='{self.name}' and level_id = {self.level} then 0 else 1 end=0; ")
+            self.level += 1
+            cur.execute(f"INSERT INTO levels (player_name,level_id) \
+                        VALUES ('{self.name}', {self.level} )")
+            # cur.execute(
+            #     f"UPDATE PLAYERS set player_level = {self.level} where player_name ='{self.name}' ")
+            # print('257')
+
+            # cur.execute(f"""SELECT player_name,CAST(AVG(level_percent) AS INTEGER) avg_percent 
+            #                 FROM levels 
+            #                 WHERE player_name = '{self.name}' 
+            #                 GROUP BY player_name;""")
+            # my_avg = cur.fetchone()
+            # cur.execute(
+            #     f"""UPDATE PLAYERS set player_level = {self.level},average_percent = {my_avg[1]},player_time = {self.count_t}
+            #     where player_name ='{self.name}' """)
+
+            
+            
+        else:
+            # 1 select click_count,if index < click_count
+            # 2 update click_count and percentage
+            # 3 level+=1
+            cur.execute(f"""
+                select click_count from levels where case when level_id ={self.level} and player_name ='{self.name}' then 0 else 1 end=0;
+                """)
+            click_x = cur.fetchone()
+
+            if self.index < click_x[0]:
+                p = int(20/self.index*100)
+                cur.execute(
+                    f"""UPDATE LEVELS set click_count = {self.index}, level_percent = {p}
+                    where case when player_name ='{self.name}' and level_id = {self.level} then 0 else 1 end=0; """)
+                cur.execute(f"""SELECT player_name,CAST(AVG(level_percent) AS INTEGER) avg_percent 
+                            FROM levels 
+                            WHERE player_name = '{self.name}' 
+                            GROUP BY player_name;""")
+                my_avg = cur.fetchone()
+                cur.execute(
+                        f"""UPDATE PLAYERS set player_level = {self.level},average_percent = {my_avg[1]},player_time = {self.count_t}
+                        where player_name ='{self.name}' """)
+                print('291')
+
+                self.level += 1
+                
+            else:
+                self.level += 1
+                print('else')
+        conn.commit()
+        conn.close()
+
+    def tebrik(self):
+        self.pushButton_2.setEnabled(False)
+        self.pushButton.setEnabled(False)
+        self.timer2.start(1000)
+        self.count = int(self.settime)
+        self.sleeptime.setText(str(self.count))
+        # append words to last of list
+        self.point2.setText(str(0))
+        self.language.setText('Next')
